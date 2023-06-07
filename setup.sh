fcac61def61aa3e20e785fb9770249c157971f2b
ShreyaSur2607 <135825776+ShreyaSur2607@users.noreply.github.com>
2023-06-07 17:58:28 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,9 @@
+mkdir -p ~.streamlit
+echo [general]
+email = shreya@stockedge.com
+  ~.streamlitcredentials.toml
+echo [server]
+headless = true
+enableCORS=false
+port = $PORT
+  ~.streamlitconfig.toml
